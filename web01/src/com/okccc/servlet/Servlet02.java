1aef9d0ed08d69ea4eb0256fa2403b2739ff65fe
okccc <1573976179@qq.com>
2024-02-20 10:25:30 +0800

    servlet生命周期

---
+++ web01/src/com/okccc/servlet/Servlet02.java
@@ -0,0 +1,46 @@
+package com.okccc.servlet;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * @Author: okccc
+ * @Date: 2023/12/2 20:45:53
+ * @Desc: Servlet对象的生命周期由Tomcat管理,后期对象都是由Spring框架创建和调用,开发人员只需关注业务
+ */
+// 查看tomcat的web.xml搜索load-on-startup发现1~5已经被占用,建议从6开始
+// 默认值-1表示tomcat启动时不会创建和初始化该Servlet,注意观察设置前后当前Servlet创建和初始化的执行时机
+@WebServlet(value = "/servlet02", loadOnStartup = 6)
+public class Servlet02 extends HttpServlet {
+
+    // 1.创建,第一次请求或服务启动时执行,1次
+    public Servlet02() {
+        System.out.println("servlet create");
+    }
+
+    // 2.初始化,构造完毕后执行,1次
+    @Override
+    public void init(ServletConfig config) {
+        System.out.println("servlet init");
+    }
+
+    // 3.服务,每次请求时执行,N次
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 创建和初始化只执行一次,说明Servlet是单例的,所以service方法不要修改成员变量,并发场景会有线程安全问题
+        System.out.println("servlet service");
+    }
+
+    // 4.销毁,服务关闭时执行,1次
+    @Override
+    public void destroy() {
+        System.out.println("servlet destroy");
+    }
+
+}
